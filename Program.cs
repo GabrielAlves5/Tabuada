5ae9a80dcd0d384d4d66c2d7d0d5959ed753b3a5
Gabrielalves5 <Gabrielalves5>
2025-05-19 08:12:25 -0300

    Tabuada com laço de repetição

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int num, tab = 1;
+Console.WriteLine("Tabuada");
+Console.WriteLine("digite um numero para saber sua tabuada:");
+num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A tabuada do {num} é:");
+while (tab < 11)
+{
+    Console.WriteLine($"{num} X {tab} = {num * tab}");
+    tab++;
+}
